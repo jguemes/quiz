5d64dad65d3a3d7d7d046b98e6c5408bc9c9ca12
jguemes <jguemes@unizar.es>
2015-06-15 17:28:42 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -39,7 +39,10 @@ sequelize.sync().success(function() {
   Quiz.count().success(function (count){
       if (count===0) {
           Quiz.create({ pregunta: 'Capital de Italia',
-                  respuesta: 'Roma'
+                respuesta: 'Roma'
+                })
+          Quiz.create({ pregunta: 'Capital de Portugal',
+                  respuesta: 'Lisboa'
                   })
     .success(function(){console.log('Base de datos inicializada')});
     };

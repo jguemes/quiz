6f5b8edd05fb269f6ebf29891470941990cb40e7
jguemes <jguemes@unizar.es>
2015-06-26 19:20:51 +0200

    Moderacion de comentarios

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,12 @@ module.exports = function(sequelize, DataTypes) {
     'Comment',
     { texto: {
       type: DataTypes.STRING,
-      validate: { notEmpty:{ msg: "--> Falta Comentario"}}
-    }}
+      validate: { notEmpty:{ msg: "--> Falta Comentario"}},
+    },
+      publicado: {
+        type : DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    }
   );
 }
